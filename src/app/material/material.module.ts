798293cee31c7cf730d5b56eedd52ab6a8cd9cce
JarniNaudts <3101jarni@gmail.com>
2019-10-24 19:34:55 +0200

    Overzicht users + responsive

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 //Material
-import {MatCardModule, MatInputModule, MatButtonModule} from '@angular/material';
+import {MatCardModule, MatInputModule, MatButtonModule, MatSortModule, MatPaginatorModule} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [],
@@ -12,12 +13,18 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatInputModule,
     MatButtonModule,
     FlexLayoutModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
   ],
   exports: [
     MatCardModule,
     MatInputModule,
     MatButtonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
   ]
 })
 export class MaterialModule { }
