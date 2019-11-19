ecd1e793842c8095fab4ff6078c0448f4b1142c5
NavaronBracke <navaron.bracke.y9090@student.hogent.be>
2019-11-19 18:24:59 +0100

    Added stub layout for add challenge

--- src/app/challenge/add-challenge/add-challenge.component.ts
+++ src/app/challenge/add-challenge/add-challenge.component.ts
@@ -1,4 +1,7 @@
+import { MatTableDataSource } from '@angular/material';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-challenge',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddChallengeComponent implements OnInit {
 
-  constructor() { }
+  public inputForm: FormGroup;
+  private _dataSource: MatTableDataSource<string> = new MatTableDataSource([
+    "Category 1","Category 2","Category 3"
+  ]);
+
+  constructor(private router: Router,private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.inputForm = this.fb.group({
+      title: ['', [Validators.required]],
+      description: ['', [Validators.required]]
+    });
+    //TODO fetch categories for list
+  }
+
+  onSubmit(){
+    //TODO: validate form input AND if category isn't null
+  }
+
+  get dataSource(){
+    return this._dataSource;
+  }
+
+  selectCategory(category) {
+    //TODO set category
   }
 
 }
