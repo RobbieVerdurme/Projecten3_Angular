be44cc808499eba92838517c1535c059087e80da
Rob Verdurme <robbie.verdurme.y9234@student.hogent.be>
2019-12-16 17:03:27 +0100

    most of the tests without mocking

--- cypress/integration/apptests/therapist/challenge.spec.js
+++ cypress/integration/apptests/therapist/challenge.spec.js
@@ -1,11 +1,36 @@
-describe('therapist challenges tests', function(){
-    beforeEach(function(){
-        cy.visit('/login');
-        cy.get('[data-cy=username]').type('TestTh');
-        cy.get('[data-cy=password]').type('P@ssword123');
-        cy.get('[data-cy=login-button]').click();
+describe("Add Challenge as Therapist", () => {
+    beforeEach(function() {
+      cy.loginTherapist();
     });
-    /*
+
+    //add challenge
+    it("Add new challenge", function(){
+        //go to add challenge
+        cy.visit("/challenge/add");
+
+        //fill in the required items
+        cy.get("[data-cy=TitleChallenge]").type("TitleChallengeTest");
+        cy.get("[data-cy=DescriptionChallenge]").type("DescriptionChallengeTest");
+
+        //click on button the make the challenge
+        cy.get("[data-cy=MakeChallenge]").click();
+
+        //go to the overview page
+
+        //check if the challenge is there
+        cy.get("[data-cy=TitleChallenge]").contains("TitleChallengeTest");
+    })
+
+    //assign challenge to user
+    it("Add challenge to user 1", function(){
+        //go to user details
+        cy.visit("/gebruiker/1");
+
+        //click on button to add challenges
+        cy.get("[data-cy=AddChallenges]").click();
+
+    });
+/*
     it('create a new challenge', function(){
         cy.server();
         cy.route({
