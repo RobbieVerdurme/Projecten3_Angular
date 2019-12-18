9a5ac86ee9d730ff3b73b4cebae2527955a20eed
Rob Verdurme <robbie.verdurme.y9234@student.hogent.be>
2019-12-18 17:23:51 +0100

    testing edit company

--- cypress/integration/apptests/Multimed/company.spec.js
+++ cypress/integration/apptests/Multimed/company.spec.js
@@ -23,14 +23,6 @@ describe("Multimed company list", () => {
     cy.get("[data-cy=CompanyPostalcode]").type("9000");
     cy.get("[data-cy=CompanyCountry]").type("Belgie");
 
-    //mock
-    cy.server();
-    cy.route({
-      method: "POST",
-      url: "/api/Companies",
-      status: 200
-    });
-
     //click on add
     cy.get("[data-cy=AddCompany]").click();
 
@@ -38,25 +30,97 @@ describe("Multimed company list", () => {
     cy.url().should("eq", "http://localhost:4200/bedrijf/lijst");
   });
 
+    //edit company
+    it("Edit company", function() {
+      //go to add company
+      cy.visit("/bedrijf/lijst");
+
+      //get the added company
+      //type in the filter
+      cy.get("[data-cy=CompanyFilter]").type("TestCompany");
+
+      //go to the company detail page
+      //click on company
+      cy.get("[data-cy=CompanyNameRow]")
+      .first()
+      .click()
+      //click on edit
+      cy.get("[data-cy=DetailEditCompany]").click();
+
+      cy.wait(5000)
+  
+      //clear the fields
+      cy.get("[data-cy=CompanyName]").clear();
+      cy.get("[data-cy=CompanySite]").clear();
+      cy.get("[data-cy=CompanyContract]").clear();
+      cy.get("[data-cy=CompanyPhone]").clear();
+      cy.get("[data-cy=CompanyEmail]").clear();
+      cy.get("[data-cy=CompanyStreet]").clear();
+      cy.get("[data-cy=CompanyHouseNumber]").clear();
+      cy.get("[data-cy=CompanyPlace]").clear();
+      cy.get("[data-cy=CompanyPostalcode]").clear();
+      cy.get("[data-cy=CompanyCountry]").clear();
+  
+      //fill in the fields
+      cy.get("[data-cy=CompanyName]").type("Multimed1");
+      cy.get("[data-cy=CompanySite]").type("Testsite");
+      cy.get("[data-cy=CompanyContract]").type("12/20/2070");
+      cy.get("[data-cy=CompanyPhone]").type("0478995886");
+      cy.get("[data-cy=CompanyEmail]").type("MultimedEmail@Test.com");
+      cy.get("[data-cy=CompanyStreet]").type("StraatNaam");
+      cy.get("[data-cy=CompanyHouseNumber]").type("28");
+      cy.get("[data-cy=CompanyPlace]").type("GentForLife");
+      cy.get("[data-cy=CompanyPostalcode]").type("9000");
+      cy.get("[data-cy=CompanyCountry]").type("BelgieEEEEEEEEEEE");
+  
+      //click on edit
+      cy.get("[data-cy=EditCompany]").click();
+  
+      //check fields
+      cy.get("[data-cy=CompanyName]").contains("Multimed1");
+      cy.get("[data-cy=CompanyPhone]").contains("0478995886");
+      cy.get("[data-cy=CompanyEmail]").contains("MultimedEmail@Test.com");
+      cy.get("[data-cy=CompanyStreet]").contains("StraatNaam 28");
+      cy.get("[data-cy=CompanyPostalcode]").contains(
+        "9000 GentForLife BelgieEEEEEEEEEEE"
+      );
+      cy.get("[data-cy=CompanyContract]").contains("12/20/2070");
+    });
+
   //Delete company
   it("Delete company", function() {
     //go to company
-    cy.visit("");
+    cy.visit("http://localhost:4200/bedrijf/lijst");
+
+    //get the added company
+    //type in the filter
+    cy.get("[data-cy=CompanyFilter]").type("Multimed1");
+
+    //go to the company detail page
+    //click on company
+    cy.get("[data-cy=CompanyNameRow]")
+    .first()
+    .click();
+
+    //click on edit en then delete
+    cy.get("[data-cy=DetailEditCompany]").click()
+    cy.get("[data-cy=DeleteCompany]").click()
+
   });
 
   //filter on company
   it("Filter on company", function() {
+        //get all companies form mockdata
+        cy.server();
+        cy.route({
+          method: "GET",
+          url: "/api/Companies",
+          status: 200,
+          response: "fixture:companies.json"
+        });
+
     //go to comany list page
     cy.visit("/bedrijf/lijst");
-
-    //get all companies form mockdata
-    cy.server();
-    cy.route({
-      method: "GET",
-      url: "/api/Companies",
-      status: 200,
-      response: "fixture:companies.json"
-    });
 
     //check if all companies are in the list
     cy.get("[data-cy=CompanyNameRow]").should("have.length", 2);
@@ -70,6 +134,15 @@ describe("Multimed company list", () => {
 
   //go to company detail
   it("Company details", function() {
+    //mock
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "/api/Companies",
+      status: 200,
+      response: "fixture:companies.json"
+    });
+
     //go to company list page
     cy.visit("/bedrijf/lijst");
 
@@ -93,59 +166,5 @@ describe("Multimed company list", () => {
     //field check
     cy.get("[data-cy=CompanyName]").contains("Multimed");
     cy.get("[data-cy=CompanyMembers]").should("have.length", 1);
-  });
-
-  //edit company
-  it("Edit company", function() {
-    //mock data company
-
-    //go to company detail page
-    cy.visit("/bedrijf/1");
-
-    //click on edit
-    cy.get("[data-cy=EditCompany]").click();
-
-    //check url
-    cy.url().should("eq", "http://localhost:4200/bedrijf/edit/1");
-
-    //clear the fields
-    cy.get("[data-cy=CompanyName]").clear();
-    cy.get("[data-cy=CompanySite]").clear();
-    cy.get("[data-cy=CompanyContract]").clear();
-    cy.get("[data-cy=CompanyPhone]").clear();
-    cy.get("[data-cy=CompanyEmail]").clear();
-    cy.get("[data-cy=CompanyStreet]").clear();
-    cy.get("[data-cy=CompanyHouseNumber]").clear();
-    cy.get("[data-cy=CompanyPlace]").clear();
-    cy.get("[data-cy=CompanyPostalcode]").clear();
-    cy.get("[data-cy=CompanyCountry]").clear();
-
-    //fill in the fields
-    cy.get("[data-cy=CompanyName]").type("Multimed1");
-    cy.get("[data-cy=CompanySite]").type("Testsite");
-    cy.get("[data-cy=CompanyContract]").type("12/20/2070");
-    cy.get("[data-cy=CompanyPhone]").type("0478995886");
-    cy.get("[data-cy=CompanyEmail]").type("MultimedEmail@Test.com");
-    cy.get("[data-cy=CompanyStreet]").type("StraatNaam");
-    cy.get("[data-cy=CompanyHouseNumber]").type("28");
-    cy.get("[data-cy=CompanyPlace]").type("GentForLife");
-    cy.get("[data-cy=CompanyPostalcode]").type("96400");
-    cy.get("[data-cy=CompanyCountry]").type("BelgieEEEEEEEEEEE");
-
-    //click on edit
-    cy.get("[data-cy=EditCompany]").click();
-
-    //redirect
-    cy.url().should("eq", "http://localhost:4200/bedrijf/1");
-
-    //check fields
-    cy.get("[data-cy=CompanyName]").contains("Multimed1");
-    cy.get("[data-cy=CompanyPhone]").contains("0478995886");
-    cy.get("[data-cy=CompanyEmail]").contains("MultimedEmail@Test.com");
-    cy.get("[data-cy=CompanyStreet]").contains("StraatNaam 28");
-    cy.get("[data-cy=CompanyPostalcode]").contains(
-      "96400 GentForLife BelgieEEEEEEEEEEE"
-    );
-    cy.get("[data-cy=CompanyContract]").contains("12/20/2070");
   });
 });
