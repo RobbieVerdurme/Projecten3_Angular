fa38f28936ba376a9e25551d277f3a70b70850bc
ArnoBoel <arno-boel@hotmail.be>
2019-12-19 13:43:02 +0100

    List of challenges of user in user-detail screen works

--- src/app/user/normal-user/normal-user-detail/normal-user-detail.component.ts
+++ src/app/user/normal-user/normal-user-detail/normal-user-detail.component.ts
@@ -6,7 +6,8 @@ import { NormalUser } from '../NormalUser';
 import { Category } from 'src/app/challenge/Category';
 import { SelectUserService } from 'src/app/challenge/select-user.service';
 import { ChallengeService } from 'src/app/challenge/challenge.service';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-normal-user-detail',
@@ -16,11 +17,17 @@ import { Observable } from 'rxjs';
 export class NormalUserDetailComponent implements OnInit {
 //var
   public user : NormalUser;
+
+  public challenges: Challenge[];
+  public errorMsg$: Subject<string> = new Subject<string>();
+  public isLoading$: Subject<boolean> = new Subject<boolean>();
   constructor(private challengeService: ChallengeService, private route: ActivatedRoute, private router: Router, private messageService: MessageService, private selectUserService: SelectUserService) { 
   }
 
   ngOnInit() {
+    this.isLoading$.next(false);
     this.LoadUser()
+    this.loadChallenges();
   }
 
   navigateToAssignChallenges(){
@@ -33,5 +40,32 @@ export class NormalUserDetailComponent implements OnInit {
 
   async LoadUser(){
     this.route.data.subscribe(item => this.user = item['user'])
+  }
+
+  loadChallenges()
+  {
+    this.isLoading$.next(true);
+    this.challengeService.getChallengesForUser(this.user.id).subscribe(
+      response =>
+      {
+        if(response.status === 200)
+        {
+          this.challenges = response.body.map((challengeUser: any) : Challenge => Challenge.fromJSONChallengeUser(challengeUser));
+          console.log("Geslaagd");
+        }
+        else
+        {
+          this.errorMsg$.next("Er liep iets fout, de challenges van de user konden niet opgehaald worden.");
+        }
+      },
+      (err: HttpErrorResponse) => {
+        this.isLoading$.next(false)
+        if(err.error instanceof Error){
+          this.errorMsg$.next(`Er zijn geen uitdagingen gevonden voor deze combinatie`);
+        }else{
+          this.errorMsg$.next(`Er zijn geen uitdagingen gevonden voor deze combinatie`);
+        }
+      }
+    )
   }
 }
