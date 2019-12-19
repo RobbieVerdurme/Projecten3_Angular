4fef470c037762006dc723408557afdcd8a248f5
ArnoBoel <arno-boel@hotmail.be>
2019-12-19 04:29:43 +0100

    Delete user-filter-pipe

--- src/app/user/normal-user/normal-user-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NormalUserFilterPipe } from './normal-user-filter.pipe';
-
-describe('NormalUserFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NormalUserFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
