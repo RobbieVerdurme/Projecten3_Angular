90ee59fbb5ba4103c6165fcad38586f2285350f2
JarniNaudts <3101jarni@gmail.com>
2019-12-20 15:05:12 +0100

    first test failes

--- cypress/integration/apptests/Multimed/user.spec.js
+++ cypress/integration/apptests/Multimed/user.spec.js
@@ -9,17 +9,35 @@ describe("Multimed Add user", () => {
         cy.visit("/gebruiker/registreren");
 
         //fil in the fields
-        cy.get("[data-cy=UserUsername]").type("TestUser");
         cy.get("[data-cy=UserFirstname]").type("User");
         cy.get("[data-cy=UserLastname]").type("Test");
         cy.get("[data-cy=UserEmail]").type("TestUser@mail.com");
-        cy.get("[data-cy=UserPhone]").type("047899556");
-        cy.get("[data-cy=UserCategory]").type("Ondergewicht");
+        cy.get("[data-cy=UserPhone]").type("047899556");  
+        cy.get("[data-cy=UserCategories]").click();
+        cy.get("[data-cy=UserCategoriesOption]").first().click();
+        cy.get("[data-cy=UserCompany]").click();    
+        cy.get("[data-cy=UserCompanyOption]").first().click();
+        cy.get("[data-cy=UserTherapists]").click();
+        cy.get("[data-cy=UserTherapistsOption]").first().click();
+
 
         //click register
         cy.get("[data-cy=UserRegister]").click();
 
         //TODO SHOULD URL REDIRECT ?
         cy.url().should("eq", "http://localhost:4200/gebruiker/lijst");
+    });
+
+    it("Delete user", function() {
+
+      cy.visit("/gebruiker/lijst");
+
+      //go to therapist
+      cy.get("[data-cy=normalUserRow]")
+        .first()
+        .click();
+  
+      //check fields
+      cy.get("[data-cy=DetailDeleteUser]").click();
     });
 });
